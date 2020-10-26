10b6cc5496b5e1c8773c9e3107dc78123e433a6e
Topchyi Nykolai <nodeTempest@gmail.com>
2020-10-26 19:24:19 +0200

    [Feature] add lodash and make items scrollable

--- src/components/AuthorsContainer.tsx
+++ src/components/AuthorsContainer.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { FlatList } from "react-native";
 import { AuhtorItem } from "./AuhtorItem";
+
+import _ from "lodash";
+
+const item = {
+  name: "James Smith",
+  email: "johndoe@mail.com",
+  posts: 5,
+};
+
+const data = _.range(0, 10).map(id => ({
+  ...item,
+  id: id.toString(),
+}));
 
 export const AuthorsContainer: React.FC = () => {
   return (
-    <View>
-      <AuhtorItem />
-      <AuhtorItem />
-      <AuhtorItem />
-    </View>
+    <FlatList
+      data={data}
+      renderItem={() => <AuhtorItem />}
+      keyExtractor={item => item.id}
+      style={{ marginRight: -18, paddingRight: 18 }}
+    />
   );
 };
