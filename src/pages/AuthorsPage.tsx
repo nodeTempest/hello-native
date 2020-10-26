b9ad378953dc89fa40fcaa71f29c2492ee829244
Topchyi Nykolai <nodeTempest@gmail.com>
2020-10-27 00:07:57 +0200

    [Feature] add dark themed status bar

--- src/pages/AuthorsPage.tsx
+++ src/pages/AuthorsPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { StatusBar } from "react-native";
 
 import { AuthorsContainer, Header, RootContainer } from "../components";
+import { theme } from "../styles";
 
 export const AuthorsPage: React.FC = () => {
   return (
     <RootContainer>
+      <StatusBar backgroundColor={theme.colors.greys[0]} />
       <Header />
       <AuthorsContainer />
     </RootContainer>
--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { StatusBar } from "react-native";
 
 import { PostsContainer, Header, RootContainer } from "../components";
+import { theme } from "../styles";
 
 export const PostsPage: React.FC = () => {
   return (
     <RootContainer>
+      <StatusBar backgroundColor={theme.colors.greys[0]} />
       <Header />
       <PostsContainer />
     </RootContainer>
